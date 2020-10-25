45a24e2ba1cddd1317a346aff58b651c47eee08c
Thomas Hodges <thomas@smartcontract.com>
2020-10-25 13:35:43 -0500

    Update for Solidity 6

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,12 @@
 module.exports = {
-  networks: {
-    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    }
-  },
   compilers: {
     solc: {
-      version: "0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
-      docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.6.12",
+      settings: {
        optimizer: {
-         enabled: false,
+         enabled: true,
          runs: 200
-        },
+        }
       }
     }
   }
